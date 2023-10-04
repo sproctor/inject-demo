424c3707d0b570d52a21ba70564e3ce2e57558a9
Sean Proctor <sproctor@gmail.com>
2023-10-04 12:51:52 -0400

    use view model in app

--- jvmApp/src/main/kotlin/com/seanproctor/demo/Main.kt
+++ jvmApp/src/main/kotlin/com/seanproctor/demo/Main.kt
@@ -2,19 +2,24 @@ package com.seanproctor.demo
 
 import com.seanproctor.demo.core.common.CommonModule
 import com.seanproctor.demo.core.common.create
-import com.seanproctor.demo.core.data.DataModule
 import com.seanproctor.demo.core.data.JvmDataModule
 import com.seanproctor.demo.core.data.create
 import com.seanproctor.demo.core.domain.DomainModule
 import com.seanproctor.demo.core.domain.create
+import com.seanproctor.demo.core.view.JvmViewModule
 import com.seanproctor.demo.core.view.ViewModule
 import com.seanproctor.demo.core.view.create
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 fun main() {
     val commonModule = CommonModule::class.create(Dispatchers.IO)
     val dataModule = JvmDataModule::class.create(commonModule)
     val domainModule = DomainModule::class.create(commonModule, dataModule)
-    val viewModule = ViewModule::class.create(commonModule, dataModule, domainModule)
-    println("Hello, world!")
+    val viewModule: ViewModule = JvmViewModule::class.create(commonModule, dataModule, domainModule)
+    val viewModel = viewModule.mainViewModelFactory.create()
+    runBlocking {
+        viewModel.networkStatus
+            .collect { println("Network status: $it") }
+    }
 }
